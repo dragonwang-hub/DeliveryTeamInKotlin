258a7b56fa85a84092455bad7f85247403c854c8
yunlong.wang <ylwang9627@foxmail.com>
2022-12-16 22:41:39 +0800

    yl #0 shouldAssignCardsToAllFreeDEV

--- app/src/main/kotlin/deliveryteaminkotlin/members/BA.kt
+++ app/src/main/kotlin/deliveryteaminkotlin/members/BA.kt
@@ -1,13 +1,35 @@
 package deliveryteaminkotlin.members
 
 import deliveryteaminkotlin.story.Story
+import deliveryteaminkotlin.story.StoryStatus
+import java.util.LinkedList
 import kotlin.random.Random
 
+@Suppress("UNCHECKED_CAST")
 data class BA(override val name: String) : Member() {
   override fun work() {
     val nextInt = Random.nextInt(1, 4)
     val stories = team!!.stories!!.toMutableList()
     IntRange(1, nextInt).forEach { stories.add(Story("randomCard${it}")) }
     team!!.stories = stories
+  }
+
+  fun assignReadyCards() {
+    val readyCards = team?.stories?.filter { it.status == StoryStatus.READY }.orEmpty()
+    val freeDEVs: List<DEV> = team?.getMembers { member: Member -> member is DEV && member.story == null }
+        .orEmpty() as List<DEV>
+
+    val canBeAssignedCount = readyCards.size.coerceAtMost(freeDEVs.size)
+    val canBeAssignedCards = LinkedList(readyCards.take(canBeAssignedCount))
+
+    freeDEVs.take(canBeAssignedCount).forEach {
+      val card = canBeAssignedCards.poll()
+      assignCardToDEV(card, it)
+    }
+  }
+
+  private fun assignCardToDEV(card: Story, dev: DEV) {
+    dev.story = card
+    card.status = StoryStatus.DEVELOP
   }
 }
--- app/src/test/kotlin/deliveryteaminkotlin/members/BATest.kt
+++ app/src/test/kotlin/deliveryteaminkotlin/members/BATest.kt
@@ -2,6 +2,7 @@ package deliveryteaminkotlin.members
 
 import deliveryteaminkotlin.Team
 import deliveryteaminkotlin.story.Story
+import deliveryteaminkotlin.story.StoryStatus
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -24,5 +25,34 @@ internal class BATest {
 //    team has 1~3 cards
     val storiesSize = baOne.team?.stories!!.size
     assertTrue(storiesSize in 2..4)
+  }
+
+  @Test
+  fun shouldAssignCardsToAllFreeDEV() {
+//    given
+    val baOne = BA("baOne")
+    val devOne = DEV("devOne")
+    val devTwo = DEV("devTwo")
+    val devThr = DEV("devOne")
+    devThr.story = Story("card", StoryStatus.DEVELOP)
+
+    val teamTG = Team()
+    val storyOne = Story("cardOne")
+    val storyTwo = Story("cardTwo")
+    val storyThr = Story("cardThr")
+    val stories = listOf(storyOne, storyTwo, storyThr)
+    val members = listOf(baOne, devOne, devTwo, devThr)
+    teamTG.stories = stories
+    teamTG.members = members
+    baOne.team = teamTG
+//    when
+    baOne.assignReadyCards()
+
+//    then
+    assertNotNull((teamTG.getMembers { member: Member -> member is DEV }.all { (it as DEV).story != null }))
+    assertNotNull((teamTG.members?.find { it.name == "devOne" } as DEV).story)
+    assertNotNull((teamTG.members?.find { it.name == "devTwo" } as DEV).story)
+    assertEquals(StoryStatus.DEVELOP, teamTG.stories?.find { it.card == "cardOne" }?.status)
+    assertEquals(StoryStatus.DEVELOP, teamTG.stories?.find { it.card == "cardTwo" }?.status)
   }
 }
